eb21620fa06174644536b9fca13726163e247df8
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2019-02-15 10:35:31 -0500

    Small missing tweak for Windows DDK in 3.8.1

--- libyara/strutils.c
+++ libyara/strutils.c
@@ -31,6 +31,27 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 #include <yara/strutils.h>
 
+// NO STRNLEN IN DKK
+size_t strnlen( const char *start, size_t maxlen )
+{
+    /* Determine the length of a NUL terminated string, subject
+    * to a maximum permitted length constraint.
+    */
+    const char *stop = start;
+
+    /* Scan at most maxlen bytes, seeking a NUL terminator;
+    * note that we MUST enforce the length check, BEFORE the
+    * terminator check, otherwise we could scan maxlen + 1
+    * bytes, which POSIX forbids.
+    */
+    while( ( ( stop - start ) < maxlen ) && *stop )
+        ++stop;
+
+    /* Result is the number of non-NUL bytes actually scanned.
+    */
+    return stop - start;
+}
+
 uint64_t xtoi(const char* hexstr)
 {
   size_t i;
